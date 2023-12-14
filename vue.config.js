a45e92f0cef40b3b3f406d29e4f9939d4acb3c98
ShoofDigital <marketing@shoof.co.nz>
2023-12-14 13:48:51 +1300

    First commit

---
+++ vue.config.js
@@ -0,0 +1,10 @@
+const { defineConfig } = require('@vue/cli-service')
+module.exports = defineConfig({
+  transpileDependencies: true,
+  devServer: {
+    proxy: 'https://app.salsify.com/'
+  },
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/salsify-vuetify/dist/'
+    : '/'
+})
